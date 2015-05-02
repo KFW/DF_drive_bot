d26bdc57525cfaf6c8f644c7d17c3b6181160f97
Keith Woeltje <comm@woeltje.org>
2015-05-02 16:47:11 -0500

    zero servos for alignment

---
+++ romeo_zero_servos/romeo_zero_servos.ino
@@ -0,0 +1,30 @@
+// Romeo Servo Test
+// using 7.2V HiTec servos powered by the servo power input. 
+// http://www.dfrobot.com/wiki/index.php/Romeo_V2-All_in_one_Controller_(R3)_(SKU:DFR0225)
+// for test will also use same battery to power board
+// with motor may need to power board seperately from servos and motors
+// simple sweep test
+
+#include <Servo.h>
+
+Servo wrist;
+Servo claw;
+
+const int WristPin = 8;
+const int ClawPin = 9;
+
+
+void setup() {
+  wrist.attach(WristPin);
+  claw.attach(ClawPin);
+  delay(100);
+  claw.write(0);
+  delay(3000); // time for servo to move
+  wrist.write(0);
+} // end setup()
+
+
+void loop() {
+  // everything done in setup
+  
+} // end loop()
