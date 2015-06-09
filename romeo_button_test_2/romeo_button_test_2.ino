442df708ebfda5a667b5913cd4b9e184f50ecc3e
Keith Woeltje <comm@woeltje.org>
2015-06-08 20:25:55 -0500

    button test 2 strip out old serve code and put in Serial

--- romeo_button_test_2/romeo_button_test_2.ino
+++ romeo_button_test_2/romeo_button_test_2.ino
@@ -7,25 +7,13 @@ http://www.dfrobot.com/wiki/index.php/Romeo_V2-All_in_one_Controller_(R3)_(SKU:D
 
 *******************/
 
-
-
-#include <Servo.h>
-
-Servo wrist;
-Servo claw;
-
-const int WristPin = 13;
-const int ClawPin = 12;
+const int ButtonPin = 0;
+int buttonVal = 0;
 
 
 
 void setup() {
-  wrist.attach(WristPin);
-  claw.attach(ClawPin);
-  wrist.write(90);
-  delay(1000);
-  claw.write(90);
-  delay(1000);
+  Serial.begin(9600);
 } // end setup()
 
 
@@ -34,24 +22,18 @@ void loop()
   buttonVal = analogRead(ButtonPin);
   
   if (buttonVal < 30){       // button 1
-    wrist.write(90);
-    claw.write(90);
-    delay(1000);    
+    Serial.println("Button 1");   
   }
   else if (buttonVal < 175){ // button 2
-    claw.write(0);
-    delay(1000);    
+    Serial.println("Button 2");     
   }
   else if (buttonVal < 360){  // button 3
-    claw.write(180);
-    delay(1000);
+    Serial.println("Button 2");  
   }
   else if (buttonVal < 540){  // button 4
-    wrist.write(0);
-    delay(1000);
+    Serial.println("Button 4");  
   }
   else if (buttonVal < 800){  // button 5
-    wrist.write(180);
-    delay(1000);
+    Serial.println("Button 5");  
   }
 }
