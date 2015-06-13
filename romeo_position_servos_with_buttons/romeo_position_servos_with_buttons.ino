38c0d46c233611ba6ebb8b35633c69b3e079b5e8
Keith Woeltje <comm@woeltje.org>
2015-06-13 18:02:30 -0500

    pos servo with buttons - changed methods for open/close

--- romeo_position_servos_with_buttons/romeo_position_servos_with_buttons.ino
+++ romeo_position_servos_with_buttons/romeo_position_servos_with_buttons.ino
@@ -15,62 +15,50 @@ const int ClawPin = 12;
 
 const int ButtonPin = 0;
 int buttonVal = 0;
-boolean flag = true;
+int wristPos = 90;
+int clawPos = 90;
 
 void setup() {
 //  Serial.begin(9600);
   wrist.attach(WristPin);
   claw.attach(ClawPin);
-  wrist.write(90);
-  claw.write(90);
+  wrist.write(wristPos);
+  claw.write(clawPos);
   delay(1000);
 } // end setup()
 
 
 void loop() 
 {
-  if (flag){ // verify servos working OK; runs once then flag resets off
-    wrist.write(0);
-    delay(1000);
-    claw.write(0);
-    delay(1000);
-    claw.write(180);
-    delay(1000);
-    wrist.write(180);
-    delay(1000);
-    wrist.write(90);
-    claw.write(90);
-    delay(2000);
-    flag = false;
-  }
 
   buttonVal = analogRead(ButtonPin);
   delay(10);
   buttonVal = analogRead(ButtonPin); // for stable value??
   
   if (buttonVal < 30){       // button 1
-    wrist.write(90);
-    claw.write(90);
-//    Serial.println("button 1");
+    wristPos = 90;
+    clawPos = 90;
+    wrist.write(wristPos);
+    claw.write(clawPos);
     delay(1000);    
   }
   else if (buttonVal < 200){ // button 2
-    claw.write(0);
-//    Serial.println("button 2");
-    delay(1000);    
+    // close claw
+    clawPos--;    
   }
   else if (buttonVal < 350){  // button 3
-    claw.write(180);
-//    Serial.println("button 3");
-    delay(1000);
+    //open claw
+    clawPos++;
   }
   else if (buttonVal < 600){  // button 4
-    wrist.write(0);
-//    Serial.println("button 4");
-    delay(1000);
+    // lower wrist
+    wristPos--;
   }
   else if (buttonVal < 850){  // button 5
-    wrist.write(180);
-    delay(1000);
+    // raise wrist
+    wristPos++;
   }
+  claw.write(clawPos);
+  wrist.write(wristPos);
+  delay(100);
 }
